3de641ab0face86d1a4401308f11238a7b2c2f70
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-01-07 13:54:06 +0800

    Added three commands for IDE.

--- setup/qxf-general.el
+++ setup/qxf-general.el
@@ -1,6 +1,10 @@
 (provide 'qxf-general)
 
-; (set-variable c-basic-offset 4)
+(defun qxf-open-init-el
+    ()
+    (interactive)
+    (find-file (format "%s/init.el" user-emacs-directory)))
+(define-key global-map (kbd "C-c `") 'qxf-open-init-el)
 
 (print "Printed from general package.")
 
--- setup/qxf-make.el
+++ setup/qxf-make.el
@@ -3,10 +3,30 @@
 ; (define-variable qxf-make-work-directory "/home/qixiaofeng/Documents/sandbox/hachi-mic-array")
 ; M-x set-variable RET c-basic-offset RET 4 RET
 
+(defconst qxf-mic-array-root "/home/qixiaofeng/Documents/sandbox/hachi-mic-array")
+
 (defun qxf-make-mic-array
     ()
     (interactive)
-    (shell-command "date && cd /home/qixiaofeng/Documents/sandbox/hachi-mic-array/build && make -j 8 && ./listdevs"))
+    (shell-command (format "date && cd %s/build && make -j 8 && ./listdevs" qxf-mic-array-root)))
 (define-key global-map (kbd "C-c 1") 'qxf-make-mic-array)
+
+(defun qxf-open-mic-array
+    ()
+    (interactive)
+    (delete-other-windows)		; Which is bound to "C-x 1".
+    (find-file (format "%s/src/listdevs.c" qxf-mic-array-root))
+    (shell-command "echo Make shell area.")
+    (let
+	((new-window (split-window nil -20 'below)))
+	(set-window-buffer new-window "*Shell Command Output*")
+	(shell-command "echo Initialized shell area.")))
+(define-key global-map (kbd "C-c 0") 'qxf-open-mic-array)
+
+(defun qxf-set-c-offset
+    ()
+    (interactive)
+    (set-variable 'c-basic-offset 4))
+(define-key global-map (kbd "C-c 9") 'qxf-set-c-offset)
 
 (print "Loaded qxf-make.")
