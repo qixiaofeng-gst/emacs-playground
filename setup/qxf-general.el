51b8b6ce9ebc6435d6c52f5e35ad0549eb3c0055
XiaofengQi <qi_xiaofeng@foxmail.com>
2021-01-29 13:29:00 +0800

    Formatted qxf-general.

--- setup/qxf-general.el
+++ setup/qxf-general.el
@@ -4,6 +4,7 @@
 (tool-bar-mode -1)
 (column-number-mode 1)
 (global-display-line-numbers-mode 1)
+(setq x-select-enable-clipboard t)
 
 (setq lisp-indent-offset 4)
 
@@ -14,14 +15,16 @@
     version-control t
     delete-old-versions t
     kept-new-versions 20
-    kept-old-versions 5)
+    kept-old-versions 5
+)
 
 ; TODO Move functions here.
 
 (defun qxf-open-init-el
     ()
     (interactive)
-    (find-file (format "%s/init.el" user-emacs-directory)))
+    (find-file (format "%s/init.el" user-emacs-directory))
+)
 (define-key global-map (kbd "C-c `") 'qxf-open-init-el)
 
 (print "Loaded qxf-general.")
